f4625f83c08379916abf11636bb92040cd94aa2f
gtseligas <71587056+gtseligas@users.noreply.github.com>
2020-09-20 16:06:48 +0300

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -1,13 +1,13 @@
 package sortingVisualiserPackage;
 
 //Simple class to implement integer pair, as I couldn't find a Java pair
-	public class Pair {
-		private int first;
-		private int second;
-		public Pair(int a, int b) {
-			first = a;
-			second = b;
-		}
-		public int getFirst() {return first;}
-		public int getSecond() {return second;}		
+public class Pair {
+	private int first;
+	private int second;
+	public Pair(int a, int b) {
+		first = a;
+		second = b;
 	}
+	public int getFirst() {return first;}
+	public int getSecond() {return second;}		
+}
